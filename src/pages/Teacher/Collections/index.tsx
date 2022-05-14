6f984c7e4f4dcf8d83666d3622ba54c7b65fc949
KKCORR <nattanon.hu@gmail.com>
2022-05-14 19:59:09 +0700

    add page breadcrumb in teacher collection page

--- src/pages/Teacher/Collections/index.tsx
+++ src/pages/Teacher/Collections/index.tsx
@@ -1,8 +1,15 @@
+import {
+	DatabaseOutlined,
+	HomeOutlined,
+	SettingOutlined,
+} from "@ant-design/icons"
 import { Space, Typography } from "antd"
 import React from "react"
 import { Helmet } from "react-helmet"
 import NewCollectionButton from "../../../components/NewCollectionButton"
+import PageBreadcrumb from "../../../components/PageBreadcrumb"
 import TeacherCollections from "../../../components/TeacherCollections"
+import { appRoutes } from "../../../constants/appRoutes"
 
 const CollectionsPage = () => {
 	return (
@@ -10,6 +17,17 @@ const CollectionsPage = () => {
 			<Helmet>
 				<title>Manage Collections</title>
 			</Helmet>
+			<PageBreadcrumb
+				items={[
+					{ icon: <HomeOutlined />, href: appRoutes.LANDING },
+					{ label: "collections", icon: <DatabaseOutlined /> },
+					{
+						label: "manage",
+						icon: <SettingOutlined />,
+						href: appRoutes.TEACHER_COLLECTIONS,
+					},
+				]}
+			/>
 			<Typography.Title>
 				<Space>
 					<span>Manage Collections</span>
