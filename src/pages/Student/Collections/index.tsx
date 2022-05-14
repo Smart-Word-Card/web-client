8148cf44764dcd58f7b34df94fb74e231a8a778c
KKCORR <nattanon.hu@gmail.com>
2022-05-14 16:35:54 +0700

    implement `StudentCollectionsPage`

--- src/pages/Student/Collections/index.tsx
+++ src/pages/Student/Collections/index.tsx
@@ -1,7 +1,18 @@
+import { Typography } from "antd"
 import React from "react"
+import { Helmet } from "react-helmet"
+import StudentCollections from "../../../components/StudentCollections"
 
 const CollectionsPage = () => {
-	return <div>CollectionsPage</div>
+	return <>
+	<Helmet>
+		<title>Collections</title>
+	</Helmet>
+	<Typography.Title>
+	Collections
+	</Typography.Title>
+	<StudentCollections />
+</>
 }
 
 export default CollectionsPage
