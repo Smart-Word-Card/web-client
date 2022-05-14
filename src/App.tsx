403c40516106b989602864f33a4df350b8fdd985
KKCORR <nattanon.hu@gmail.com>
2022-05-14 13:37:24 +0700

    setup axios fetcher and client

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,42 @@
 import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { SWRConfig } from "swr"
 import LandingPage from "./pages/Landing"
 import StudentCollectionsPage from "./pages/Student/Collections"
 import CollectionsPlayPage from "./pages/Student/Collections/Play"
 import TeacherCollectionsPage from "./pages/Teacher/Collections"
 import CollectionsEditPage from "./pages/Teacher/Collections/Edit"
 import CollectionsNewPage from "./pages/Teacher/Collections/New"
+import axiosFetcher from "./utils/axios/axiosFetcher"
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route index element={<LandingPage />} />
-				<Route path="teacher">
-					<Route path="collections">
-						<Route index element={<TeacherCollectionsPage />} />
-						<Route path="new" element={<CollectionsNewPage />} />
-						<Route
-							path="edit/:collectionId"
-							element={<CollectionsEditPage />}
-						/>
+		<SWRConfig value={{ fetcher: axiosFetcher, revalidateOnMount: true }}>
+			<BrowserRouter>
+				<Routes>
+					<Route index element={<LandingPage />} />
+					<Route path="teacher">
+						<Route path="collections">
+							<Route index element={<TeacherCollectionsPage />} />
+							<Route path="new" element={<CollectionsNewPage />} />
+							<Route
+								path="edit/:collectionId"
+								element={<CollectionsEditPage />}
+							/>
+						</Route>
 					</Route>
-				</Route>
-				<Route path="student">
-					<Route path="collections">
-						<Route index element={<StudentCollectionsPage />} />
-						<Route
-							path="play/:collectionId"
-							element={<CollectionsPlayPage />}
-						/>
+					<Route path="student">
+						<Route path="collections">
+							<Route index element={<StudentCollectionsPage />} />
+							<Route
+								path="play/:collectionId"
+								element={<CollectionsPlayPage />}
+							/>
+						</Route>
 					</Route>
-				</Route>
-			</Routes>
-		</BrowserRouter>
+				</Routes>
+			</BrowserRouter>
+		</SWRConfig>
 	)
 }
 
