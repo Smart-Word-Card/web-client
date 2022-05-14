d23f766075850c4cf2ed2cf0905cecc1f5812b36
KKCORR <nattanon.hu@gmail.com>
2022-05-14 20:04:57 +0700

    implement `TeacherCollectionsNewPage`

--- src/pages/Teacher/Collections/New/index.tsx
+++ src/pages/Teacher/Collections/New/index.tsx
@@ -1,7 +1,41 @@
+import {
+	DatabaseOutlined,
+	HomeOutlined,
+	PlusCircleOutlined,
+	SettingOutlined,
+} from "@ant-design/icons"
+import { Typography } from "antd"
 import React from "react"
+import { Helmet } from "react-helmet"
+import NewCollectionForm from "../../../../components/NewCollectionForm"
+import PageBreadcrumb from "../../../../components/PageBreadcrumb"
+import { appRoutes } from "../../../../constants/appRoutes"
 
 const CollectionsNewPage = () => {
-	return <div>CollectionsNewPage</div>
+	return (
+		<>
+			<Helmet>
+				<title>New Collection</title>
+			</Helmet>
+			<PageBreadcrumb
+				items={[
+					{ icon: <HomeOutlined />, href: appRoutes.LANDING },
+					{ label: "collections", icon: <DatabaseOutlined /> },
+					{
+						label: "manage",
+						icon: <SettingOutlined />,
+						href: appRoutes.TEACHER_COLLECTIONS,
+					},
+					{
+						label: "new",
+						icon: <PlusCircleOutlined />,
+					},
+				]}
+			/>
+			<Typography.Title>Create a new collection</Typography.Title>
+			<NewCollectionForm />
+		</>
+	)
 }
 
 export default CollectionsNewPage
