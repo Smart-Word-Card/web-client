81fd32e8c1e6ad983197f8191fe0c3b4a3570d7f
KKCORR <nattanon.hu@gmail.com>
2022-05-14 16:20:39 +0700

    implement `TeacherCollectionsPage`

--- src/pages/Teacher/Collections/index.tsx
+++ src/pages/Teacher/Collections/index.tsx
@@ -1,7 +1,24 @@
+import { Space, Typography } from "antd"
 import React from "react"
+import { Helmet } from "react-helmet"
+import NewCollectionButton from "../../../components/NewCollectionButton"
+import TeacherCollections from "../../../components/TeacherCollections"
 
 const CollectionsPage = () => {
-	return <div>CollectionsPage</div>
+	return (
+		<>
+			<Helmet>
+				<title>Manage Collections</title>
+			</Helmet>
+			<Typography.Title>
+				<Space>
+					<span>Manage Collections</span>
+					<NewCollectionButton />
+				</Space>
+			</Typography.Title>
+			<TeacherCollections />
+		</>
+	)
 }
 
 export default CollectionsPage
