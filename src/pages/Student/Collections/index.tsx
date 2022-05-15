04aa6069f1ff5d02f6cfac4a64ca79282cd7e92a
KKCORR <nattanon.hu@gmail.com>
2022-05-15 20:44:26 +0700

    add `PageBreadcrumb` in page

--- src/pages/Student/Collections/index.tsx
+++ src/pages/Student/Collections/index.tsx
@@ -1,18 +1,36 @@
+import {
+	DatabaseOutlined,
+	HomeOutlined,
+	PlayCircleOutlined,
+} from "@ant-design/icons"
 import { Typography } from "antd"
 import React from "react"
 import { Helmet } from "react-helmet"
+import PageBreadcrumb from "../../../components/PageBreadcrumb"
 import StudentCollections from "../../../components/StudentCollections"
+import { appRoutes } from "../../../constants/appRoutes"
 
 const CollectionsPage = () => {
-	return <>
-	<Helmet>
-		<title>Collections</title>
-	</Helmet>
-	<Typography.Title>
-	Collections
-	</Typography.Title>
-	<StudentCollections />
-</>
+	return (
+		<>
+			<Helmet>
+				<title>Play Collections</title>
+			</Helmet>
+			<PageBreadcrumb
+				items={[
+					{ icon: <HomeOutlined />, href: appRoutes.LANDING },
+					{ label: "collections", icon: <DatabaseOutlined /> },
+					{
+						label: "play",
+						icon: <PlayCircleOutlined />,
+						href: appRoutes.STUDENT_COLLECTIONS,
+					},
+				]}
+			/>
+			<Typography.Title>Collections</Typography.Title>
+			<StudentCollections />
+		</>
+	)
 }
 
 export default CollectionsPage
