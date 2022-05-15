7b5e0c70725cc2de4e96d7ec96493dcc7d413eb4
KKCORR <nattanon.hu@gmail.com>
2022-05-15 16:44:53 +0700

    remove path

--- src/constants/appRoutes.ts
+++ src/constants/appRoutes.ts
@@ -2,7 +2,6 @@ export enum appRoutes {
 	LANDING = "/",
 	TEACHER_COLLECTIONS = "/teacher/collections/",
 	TEACHER_COLLECTIONS_NEW = "/teacher/collections/new/",
-	TEACHER_COLLECTIONS_PREVIEW = "/teacher/collections/new/preview",
 	TEACHER_COLLECTIONS_EDIT = "/teacher/collections/edit/",
 	STUDENT_COLLECTIONS = "/student/collections/",
 	STUDENT_COLLECTIONS_PLAY = "/student/collections/play/",
