99d903ef60f3887e691bfc65ac71cb516626df7e
KKCORR <nattanon.hu@gmail.com>
2022-05-15 14:12:33 +0700

    add proxy server for development

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+	plugins: [react()],
+	server: {
+		host: "0.0.0.0",
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ""),
+			},
+		},
+	},
 })
