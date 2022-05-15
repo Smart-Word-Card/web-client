161f49bafb6ea0d76507bd074d736c74ed03f332
KKCORR <nattanon.hu@gmail.com>
2022-05-15 18:14:15 +0700

    implement `CollectionsEditPage`

--- src/pages/Teacher/Collections/Edit/index.tsx
+++ src/pages/Teacher/Collections/Edit/index.tsx
@@ -1,7 +1,41 @@
+import {
+	DatabaseOutlined,
+	EditOutlined,
+	HomeOutlined,
+	SettingOutlined,
+} from "@ant-design/icons"
+import { Typography } from "antd"
 import React from "react"
+import { Helmet } from "react-helmet"
+import EditCollectionForm from "../../../../components/EditCollectionForm"
+import PageBreadcrumb from "../../../../components/PageBreadcrumb"
+import { appRoutes } from "../../../../constants/appRoutes"
 
 const CollectionsEditPage = () => {
-	return <div>CollectionsEditPage</div>
+	return (
+		<>
+			<Helmet>
+				<title>Edit Collection</title>
+			</Helmet>
+			<PageBreadcrumb
+				items={[
+					{ icon: <HomeOutlined />, href: appRoutes.LANDING },
+					{ label: "collections", icon: <DatabaseOutlined /> },
+					{
+						label: "manage",
+						icon: <SettingOutlined />,
+						href: appRoutes.TEACHER_COLLECTIONS,
+					},
+					{
+						label: "edit",
+						icon: <EditOutlined />,
+					},
+				]}
+			/>
+			<Typography.Title>Edit Collection</Typography.Title>
+			<EditCollectionForm />
+		</>
+	)
 }
 
 export default CollectionsEditPage
